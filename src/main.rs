796cd785d715979e3cce796e4f16668716989531
mendess <pedro.mendes.26@gmail.com>
2023-06-04 20:14:40 +0100

    v1

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+mod react_here;
+
+use futures::FutureExt;
+use pubsub::ControlFlow;
+use serde::{Deserialize, Serialize};
+use serenity::{
+    model::application::{
+        command::Command,
+        interaction::{Interaction, InteractionResponseType},
+    },
+    prelude::GatewayIntents,
+    Client,
+};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Config {
+    token: String,
+}
+
+async fn register_integration_commands() {
+    use pubsub::events::{InteractionCreate, Message, Ready};
+
+    pubsub::subscribe::<InteractionCreate, _>(|ctx, interaction| {
+        async move {
+            if let Interaction::ApplicationCommand(command) = interaction {
+                let react_here::CMD_NAME = command.data.name.as_str() else {
+                    return ControlFlow::CONTINUE;
+                };
+                let result = match command.guild_id {
+                    Some(id) => react_here::run(&command.data.options, id).await,
+                    None => Err(anyhow::anyhow!("this command can only be run in a server")),
+                };
+                if let Err(why) = command
+                    .create_interaction_response(ctx, |resp| {
+                        resp.kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|msg| {
+                                msg.content(match result {
+                                    Ok(_) => "done".into(),
+                                    Err(e) => e.to_string(),
+                                })
+                            })
+                    })
+                    .await
+                {
+                    println!("failed to respond to slash command: {why}");
+                }
+            }
+            ControlFlow::CONTINUE
+        }
+        .boxed()
+    })
+    .await;
+
+    pubsub::subscribe::<Ready, _>(|ctx, _ready| {
+        async move {
+            if let Err(why) =
+                Command::create_global_application_command(ctx, react_here::register).await
+            {
+                eprintln!(
+                    "failed to register {} command: {why:?}",
+                    react_here::CMD_NAME
+                );
+            };
+            ControlFlow::BREAK
+        }
+        .boxed()
+    })
+    .await;
+
+    pubsub::subscribe::<Message, _>(|ctx, message| {
+        async move {
+            if let Err(why) = react_here::react_to(message, ctx).await {
+                eprintln!("failed to react to message: {why:?}");
+            }
+            ControlFlow::CONTINUE
+        }
+        .boxed()
+    })
+    .await;
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config: Config = config::Config::builder()
+        .add_source(config::File::with_name("./config"))
+        .build()?
+        .try_deserialize()?;
+
+    let mut client = Client::builder(
+        config.token,
+        GatewayIntents::GUILD_INTEGRATIONS
+            .union(GatewayIntents::GUILD_MESSAGE_REACTIONS)
+            .union(GatewayIntents::GUILD_MESSAGES),
+    )
+    .event_handler(pubsub::event_handler::Handler::new(None))
+    .await?;
+
+    register_integration_commands().await;
+
+    pubsub::subscribe::<pubsub::events::Ready, _>(|_, ready| {
+        async move {
+            println!(
+                "Invite me https://discord.com/oauth2/authorize?client_id={}&scope=bot",
+                ready.user.id
+            );
+            ControlFlow::BREAK
+        }
+        .boxed()
+    })
+    .await;
+
+    if let Err(e) = client.start().await {
+        anyhow::bail!("client error: {e:?}")
+    }
+    Ok(())
+}
---
+++ src/react_here.rs
@@ -0,0 +1,95 @@
+use std::{collections::HashMap, sync::OnceLock};
+
+use anyhow::anyhow;
+use json_db::GlobalDatabase;
+use serenity::{
+    builder::CreateApplicationCommand,
+    model::{
+        id::{ChannelId, GuildId},
+        prelude::{
+            command::CommandOptionType, interaction::application_command::CommandDataOption,
+            EmojiId, Message, ReactionType,
+        },
+        Permissions,
+    },
+    prelude::Context,
+};
+
+pub const CMD_NAME: &str = "react-here";
+
+static GUILDS: GlobalDatabase<HashMap<GuildId, ChannelId>> =
+    json_db::Database::const_new("guilds.json");
+
+pub async fn react_to(msg: &Message, ctx: &Context) -> anyhow::Result<()> {
+    async fn should_react(msg: &Message) -> anyhow::Result<bool> {
+        let Some(gid) = msg.guild_id else {
+                return Ok(false)
+            };
+        Ok(GUILDS
+            .load()
+            .await?
+            .get(&gid)
+            .is_some_and(|id| id == &msg.channel_id))
+    }
+
+    static REACTIONS: OnceLock<[ReactionType; 5]> = OnceLock::new();
+
+    let reactions = || {
+        REACTIONS.get_or_init(|| {
+            [
+                ReactionType::Unicode("❤️".into()),
+                ReactionType::Unicode("👍".into()),
+                ReactionType::Unicode("👌".into()),
+                ReactionType::Unicode("👎".into()),
+                ReactionType::Custom {
+                    animated: false,
+                    id: EmojiId(1114991930099642389),
+                    name: Some("dust".into()),
+                },
+            ]
+        })
+    };
+
+    if should_react(msg).await? {
+        for reaction in reactions() {
+            eprintln!("reacting with {reaction}");
+            msg.react(ctx, reaction.clone()).await?;
+        }
+    }
+    Ok(())
+}
+
+pub async fn run(options: &[CommandDataOption], guild_id: GuildId) -> anyhow::Result<()> {
+    let channel_id: ChannelId = options[0]
+        .value
+        .as_ref()
+        .ok_or_else(|| anyhow!("argument not provided"))?
+        .as_str()
+        .and_then(|s| s.parse::<u64>().ok())
+        .ok_or_else(|| anyhow!("argument was not a channelId"))?
+        .into();
+
+    GUILDS
+        .load()
+        .await?
+        .entry(guild_id)
+        .and_modify(|ch| *ch = channel_id)
+        .or_insert(channel_id);
+
+    Ok(())
+}
+
+pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command
+        .name(CMD_NAME)
+        .description("set the channel the bot will react to")
+        .dm_permission(false)
+        .default_member_permissions(Permissions::MANAGE_GUILD)
+        .create_option(|option| {
+            option
+                .name("channel")
+                .description("the channel the bot will react to")
+                .kind(CommandOptionType::Channel)
+                .required(true)
+        })
+}
